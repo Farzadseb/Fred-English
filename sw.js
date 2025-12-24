09f76484f6d3bf75005bd1662a098db3b8f39747
Farzadseb <farzad.seb@gmail.com>
2025-12-24 07:28:49 +0330

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,41 @@
+// Service Worker برای آفلاین
+const CACHE_NAME = 'english-fred-v1.0';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/dic.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
