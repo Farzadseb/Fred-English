fb5a1b70c0b12d75478db574f6bacd74d3554818
Farzadseb <farzad.seb@gmail.com>
2025-12-25 00:18:23 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,25 @@
-const CACHE = 'fred-v3';
+const CACHE_NAME = 'english-fred-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './words.js'
+];
 
-self.addEventListener('install',e=>{
-  e.waitUntil(
-    caches.open(CACHE).then(c=>c.addAll([
-      './','./index.html','./words.js'
-    ]))
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
   );
-  self.skipWaiting();
 });
 
-self.addEventListener('activate',e=>{
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch',e=>{
-  e.respondWith(
-    caches.match(e.request).then(r=>r||fetch(e.request))
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
   );
 });
