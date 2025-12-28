85fb1e0ae5906938fe43678210e806be68def848
Farzadseb <farzad.seb@gmail.com>
2025-12-29 02:55:28 +0330

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,22 +1,23 @@
-(function () {
-    const v = Date.now();
+(() => {
+  const v = Date.now();
 
-    const scripts = [
-        'words.js',
-        'speech.js',
-        'quiz.js',
-        'app.js'
-    ];
+  const files = [
+    'words.js',
+    'speech.js',
+    'quiz.js',
+    'app.js',
+    'telegram.js'
+  ];
 
-    scripts.forEach(f => {
-        const s = document.createElement('script');
-        s.src = `${f}?v=${v}`;
-        s.defer = true;
-        document.body.appendChild(s);
-    });
+  files.forEach(f => {
+    const s = document.createElement('script');
+    s.src = f + '?v=' + v;
+    s.defer = true;
+    document.body.appendChild(s);
+  });
 
-    const css = document.createElement('link');
-    css.rel = 'stylesheet';
-    css.href = `style.css?v=${v}`;
-    document.head.appendChild(css);
+  const css = document.createElement('link');
+  css.rel = 'stylesheet';
+  css.href = 'style.css?v=' + v;
+  document.head.appendChild(css);
 })();
