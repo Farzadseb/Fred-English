cd02041a27a005050d36c847c2a70c0ae9a1af69
Farzadseb <farzad.seb@gmail.com>
2026-01-01 23:36:27 +0330

    Delete telegram-config.js

--- telegram-config.js
+++
@@ -1,9 +0,0 @@
-// =======================
-// TELEGRAM CONFIG
-// =======================
-
-window.TelegramConfig = {
-    BOT_TOKEN: '8592902186:AAGdV2eHkocXaRr7kKrxLrap7jWVPm0pq-Q',
-    CHAT_ID: '96991859',
-    API_URL: 'https://api.telegram.org/bot'
-};
