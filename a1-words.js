2db758d977676fdfaae46c5e28ec438bdaae3567
Farzadseb <farzad.seb@gmail.com>
2026-01-05 15:41:19 +0330

    Update a1-words.js

--- a1-words.js
+++ a1-words.js
@@ -7,15 +7,9 @@ window.wordsA1 = [
     ex_trans: "او سال‌ها تجربه در تدریس دارد.",
     collocation: "Work experience",
     coll_trans: "تجربه کاری",
-    coll_ex: "I need to gain more experience.",
-    coll_ex_trans: "من باید تجربه بیشتری کسب کنم.",
     phrasal_1: "Go through",
-    ph1_trans: "تجربه کردن (سختی)",
-    ph1_ex: "He is going through a lot.",
-    ph1_ex_trans: "او دوران سختی را می‌گذراند.",
+    ph1_trans: "تجربه کردن (شرایط سخت)",
     phrasal_2: "Build up",
-    ph2_trans: "تقویت کردن",
-    ph2_ex: "Build up your skills.",
-    ph2_ex_trans: "مهارت‌هایت را تقویت کن."
+    ph2_trans: "تقویت کردن"
   }
 ];
